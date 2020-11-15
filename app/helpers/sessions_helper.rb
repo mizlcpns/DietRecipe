56af22a17728ac1151b98324052c32b3534b64f0
mizuki.tsujiuchi <mizlcpns@gmail.com>
2020-11-15 08:03:00 +0000

    user login

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,10 @@
+module SessionsHelper
+  def current_user
+    #現在ログインしているユーザーを取得
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+end
